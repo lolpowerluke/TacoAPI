5d3920df043db4e1da42209ff0bb71f4678d2e88
RobinRoeland <123749343+RobinRoeland@users.noreply.github.com>
2023-03-27 18:13:30 +0200

    Add files via upload
    First working API

---
+++ tacoAPI/tacoapi/src/test/java/group/tacoapi/tacoapi/TacoapiApplicationTests.java
@@ -0,0 +1,13 @@
+package group.tacoapi.tacoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
